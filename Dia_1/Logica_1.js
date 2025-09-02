f8640f9cf62a3d89ca70736808260d275815c5c2
Santiago Noe <01sistemas2019@gmail.com>
2025-09-02 16:56:03 -0500

    Día 1 - lógica de programación en JS

---
+++ Dia_1/Logica_1.js
@@ -0,0 +1,28 @@
+let numeroUn  = 1
+let stringUn = "1"
+
+let numeroTreinta = 30
+let stringTreinta = "30"
+
+let numeroDiez = 10
+let stringDiez = "10"
+
+
+if (numeroUn === numeroUn) {
+  console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes');
+} else {
+    console.log('Las variables numeroUn y stringUn no tienen el mismo valor'); 
+}
+
+
+if (numeroTreinta === numeroTreinta) {
+    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor, pero tipos diferentes');
+} else {
+    console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo valor');
+}   
+
+if (numeroDiez === numeroDiez) {
+    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes');
+} else {
+    console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor');
+}

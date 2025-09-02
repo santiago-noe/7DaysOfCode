10ebc69c87650f5e4bc87fe4b9024f6a40c90eff
Santiago Noe <01sistemas2019@gmail.com>
2025-09-02 17:05:17 -0500

    Dia uno de Logica de programacion

--- Dia_1/Logica_1.js
+++ Dia_1/Logica_1.js
@@ -7,22 +7,26 @@ let stringTreinta = "30"
 let numeroDiez = 10
 let stringDiez = "10"
 
-
-if (numeroUn === numeroUn) {
+if (numeroUn === stringUn) {
+  console.log('Las variables numeroUn y stringUn tienen el mismo valor y el mismo tipo');
+} else if (numeroUn == stringUn) {
   console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes');
 } else {
-    console.log('Las variables numeroUn y stringUn no tienen el mismo valor'); 
+  console.log('Las variables numeroUn y stringUn no tienen el mismo valor');
 }
 
-
-if (numeroTreinta === numeroTreinta) {
-    console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor, pero tipos diferentes');
+if (numeroTreinta === stringTreinta) {
+  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo');
+} else if (numeroTreinta == stringTreinta) {
+  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor, pero tipos diferentes');
 } else {
-    console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo valor');
-}   
+  console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo valor');
+}
 
-if (numeroDiez === numeroDiez) {
-    console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes');
+if (numeroDiez === stringDiez) {
+  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor y el mismo tipo');
+} else if (numeroDiez == stringDiez) {
+  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes');
 } else {
-    console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor');
+  console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor');
 }

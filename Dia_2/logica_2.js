ce2569ef44b6f6fde66b5cca133ef319fc7c4c24
Santiago Noe <01sistemas2019@gmail.com>
2025-09-03 18:34:38 -0500

    segundo dia del reto

--- Dia_2/logica_2.js
+++ Dia_2/logica_2.js
@@ -1,5 +1,18 @@
- const nombre = prompt("¿Cuál es tu nombre?");
- const edad = prompt("¿Cuál es tu edad?");
- const ciudad = prompt("¿Cuál es tu ciudad?");
+alert("¡Bienvenido!");
 
- 
+//const nombre = prompt("¿Cuál es tu nombre?");
+//const edad = prompt("¿Cuál es tu edad?");
+//const ciudad = prompt("¿Cuál es tu ciudad?");
+
+
+//console.log(`Hola, mi nombre es ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
+
+const formulario = document.getElementById('formulario');
+        const resultado = document.getElementById('resultado');
+        formulario.addEventListener('submit', function(e) {
+            e.preventDefault();
+            const nombre = document.getElementById('nombre').value;
+            const edad = document.getElementById('edad').value;
+            const ciudad = document.getElementById('ciudad').value;
+            resultado.innerHTML = `Hola, mi nombre es <strong>${nombre}</strong>, tengo <strong>${edad}</strong> años y vivo en <strong>${ciudad}</strong>.`;
+        });

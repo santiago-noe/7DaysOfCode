31cc02d8d1f06295198f2887750f333515abab0f
Santiago Noe <01sistemas2019@gmail.com>
2025-09-03 19:19:04 -0500

    dia dos de lña logica de progrmacion

--- Dia_2/logica_2.js
+++ Dia_2/logica_2.js
@@ -1,10 +1,19 @@
 alert("¡Bienvenido!");
 
-//const nombre = prompt("¿Cuál es tu nombre?");
-//const edad = prompt("¿Cuál es tu edad?");
-//const ciudad = prompt("¿Cuál es tu ciudad?");
+const nombre = prompt("¿Cuál es tu nombre?");
+const edad = prompt("¿Cuántos años tienes?");
+const lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?");
+const mensaje = "¡Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!";
 
+alert(mensaje);
 
+const gusta = prompt("¿Te gusta estudiar ${lenguaje}? Responde con el número 1 - SÍ o 2 - NO");
+if (gusta == 1){
+    alert("¡Muy bien! Sigue estudiando y tendrás mucho éxito.");
+}
+if (gusta == 2){
+    alert("Oh, qué pena... ¿Has intentado aprender otros lenguajes?");
+}
 //console.log(`Hola, mi nombre es ${nombre}, tengo ${edad} años y vivo en ${ciudad}.`);
 
 const formulario = document.getElementById('formulario');
